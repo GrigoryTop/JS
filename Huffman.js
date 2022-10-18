062d9dcb1abeec0e924e8310247affc2666e1f34
GrigoryTop <114752896+GrigoryTop@users.noreply.github.com>
2022-10-18 19:23:02 +0500

    Update Huffman.js

--- Huffman.js
+++ Huffman.js
@@ -1,86 +1,99 @@
 let inpStr = 'abrakadabra'; 
 let alph = new Array(); 
- 
+let check = 0;
+
 for (let i=0; i<inpStr.length; i++) {//Считаем частоту символов строки
 	if (inpStr.charAt(i) in alph){
 		alph[inpStr.charAt(i)]++;
 	}
 	else {
 		alph[inpStr.charAt(i)] = 1;
+		check += 1;
 	}
 }
+let code;
+let decodStr = '';
 
-let array_alph = new Array;
-for (i in alph) {
-	array_alph.push([i, alph[i]]);
-}
-
-function sortArray(a,b){
-	if (a[1]>b[1]) return 1;
-	if (a[1]==b[1]) return 0;
-	if (a[1]<b[1]) return -1;
-}
-array_alph.sort(sortArray); //Отсортированный массив с частотами
-
-function buildtree(tree) {
-	while (tree.length > 1) {
-		let branch = [tree[0][0],tree[1][0]];
-		
-		let cymm_branch = tree[0][1]+tree[1][1];
-		
-		let rest = tree.slice(2,tree.length);
-		
-		let end = [branch, cymm_branch];
-		tree = rest;
-		tree.push(end);
-		tree.sort(sortArray);
+if (check == 1){  // Проверка на 1 символ в строке
+	code = 0;
+	for (i in inpStr){
+		decodStr += inpStr.charAt(i);
 	}
-	return tree[0][0]
-}
-let Tree = buildtree(array_alph);
-
-console.log(Tree);//Дерево в виде массива
-
-code={};  
-pat='';
-function assignCode(node,pat)  { //Строим по дереву коды символов
-    if (typeof(node)==typeof("")){
-        code[node]=pat;
-    } else { 
-        assignCode( node[0], pat+'0');  
-        assignCode( node[1], pat+'1');  
-    }
-}  
-assignCode(Tree,pat);  
-
-console.log(code); //Итоговые коды символов
-
-let array_code = new Array;
-for (i in code) {
-	array_code.push([i, code[i]]);
-}
-
-let result = ''
-for (i = 0; i<inpStr.length; i++){  //кодирование входящей строки
-	for (j in array_code){
-		if (inpStr[i]==array_code[j][0]){
-		result += array_code[j][1]; }
+	console.log('a:', code);
+	console.log(decodStr);
+	
+}else{  // Если символов больше 1, делаем все остальное
+	let array_alph = new Array;
+	for (i in alph) {
+		array_alph.push([i, alph[i]]);
 	}
-}
-console.log(result);   //итоговая закодированная строка
+	console.log(alph);
+	function sortArray(a,b){
+		if (a[1]>b[1]) return 1;
+		if (a[1]==b[1]) return 0;
+		if (a[1]<b[1]) return -1;
+	}
+	array_alph.sort(sortArray); //Отсортированный массив с частотами
 
+	function buildtree(tree) {
+		while (tree.length > 1) {
+			let branch = [tree[0][0],tree[1][0]];
+			
+			let cymm_branch = tree[0][1]+tree[1][1];
+			
+			let rest = tree.slice(2,tree.length);
+			
+			let end = [branch, cymm_branch];
+			tree = rest;
+			tree.push(end);
+			tree.sort(sortArray);
+		}
+		return tree[0][0]
+	}
+	let Tree = buildtree(array_alph);
 
+	console.log(Tree);//Дерево в виде массива
 
-let decodStr= '';
-let decoder = '';
-for (i =0; i < result.length; i++){
-	decoder += result[i];
-	for (j in array_code){
-		if (decoder == array_code[j][1]){
-			decodStr += array_code[j][0];
-			decoder = '';
+	code={};  
+	pat='';
+	function assignCode(node,pat)  { //Строим по дереву коды символов
+		if (typeof(node)==typeof("")){
+			code[node]=pat;
+		} else { 
+			assignCode( node[0], pat+'0');  
+			assignCode( node[1], pat+'1');  
+		}
+	}  
+	assignCode(Tree,pat);  
+
+	console.log(code); //Итоговые коды символов
+
+	let array_code = new Array;
+	for (i in code) {
+		array_code.push([i, code[i]]);
+	}
+
+	let result = ''
+	for (i = 0; i<inpStr.length; i++){  //кодирование входящей строки
+		for (j in array_code){
+			if (inpStr[i]==array_code[j][0]){
+			result += array_code[j][1]; }
 		}
 	}
-}
+	console.log(result);   //итоговая закодированная строка
 
-console.log(decodStr); //декодированная обратно строка
+
+
+	let decodStr= '';
+	let decoder = '';
+	for (i =0; i < result.length; i++){
+		decoder += result[i];
+		for (j in array_code){
+			if (decoder == array_code[j][1]){
+				decodStr += array_code[j][0];
+				decoder = '';
+			}
+		}
+	}
+	console.log(decodStr);
+}
