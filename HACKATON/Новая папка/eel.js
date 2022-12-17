68c277a63f02eae05d8609b05f06caca2e47418f
GrigoryTop <114752896+GrigoryTop@users.noreply.github.com>
2022-12-17 15:23:01 +0500

    Add files via upload

---
+++ HACKATON/Новая папка/eel.js
@@ -0,0 +1,25 @@
+let input = {'RECOMMENDATION': 'BUY',
+            'BUY': 2,
+            'SELL': 1,
+            'NEUTRAL': 8,
+            'COMPUTE': {'RSI': 'NEUTRAL',
+                        'STOCH.K': 'NEUTRAL', 
+                        'CCI': 'NEUTRAL', 
+                        'ADX': 'NEUTRAL', 
+                        'AO': 'NEUTRAL', 
+                        'Mom': 'BUY', 
+                        'MACD': 'SELL', 
+                        'Stoch.RSI': 'NEUTRAL', 
+                        'W%R': 'NEUTRAL', 
+                        'BBP': 'BUY', 
+                        'UO': 'NEUTRAL'}};
+
+function addElement() {
+    let newDiv = document.createElement("div");
+        newDiv.innerHTML = "<h1>Привет!</h1>";
+}
+
+
+
+
+
