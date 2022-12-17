32fe5b76cc0b914623e227d56dfb300763632df3
GrigoryTop <114752896+GrigoryTop@users.noreply.github.com>
2022-12-17 15:37:37 +0500

    Add files via upload

---
+++ HACKATON/js/scriptForBRS.js
@@ -0,0 +1,27 @@
+localStorage.setItem('risk', 0);
+localStorage.setItem('experience', 0);
+localStorage.setItem('reliability', 0);
+
+function q1a1(){
+    localStorage.risk = parseInt(localStorage.risk) + 0;
+}
+
+function q1a2(){
+    localStorage.risk = parseInt(localStorage.risk) + 0;
+}
+
+function q1a3(){
+    localStorage.risk = parseInt(localStorage.risk) + 0;
+}
+
+function q1a4(){
+    localStorage.risk = parseInt(localStorage.risk) + 0;
+}
+
+function q1a5(){
+    localStorage.risk = parseInt(localStorage.risk) + 0;
+}
+
+function q1a6(){
+    localStorage.risk = parseInt(localStorage.risk) + 0;
+}
